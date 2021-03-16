492e7d587b366b3f983fa128f67cc27cc41e8922
Ola Assem <ola.assem@viget.com>
2021-03-17 00:13:37 +0200

    adds .nvmrc

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Hello, React!</h1>
+        <h1>A Sky Full of Stars.</h1>
       </div>
     )
   }
