52b4ca19584d941bd7dc504470c7d5e3d5609f10
Ola Assem <ola.assem@viget.com>
2021-03-17 23:04:22 +0200

    initial form comp

---
+++ src/Form.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Form extends Component {
+    initialState = {
+        name: '',
+        job: ''
+    }
+    
+    state = this.initialState;
+}
+ 
+export default Form;
