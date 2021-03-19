ea2e2d66190798a7441e59d9c9775508a1092b00
Ola Assem <ola.assem@viget.com>
2021-03-19 13:07:47 +0200

    input handle change function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 
 import Table from './Table';
+import Form from './Form'
 
 class App extends Component {
     state = {
-        characters: [];
+        characters: []
     }
 
     removeCharacter = (index) => {
@@ -20,9 +21,11 @@ class App extends Component {
     render() {
       return (
         <div className="container">
-          <Table 
-            characterData={this.state.characters}
-            removeCharacter={this.removeCharacter} />
+            <Table 
+                characterData={this.state.characters}
+                removeCharacter={this.removeCharacter} 
+            />
+            <Form />
         </div>
       )
     }
